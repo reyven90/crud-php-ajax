73ec18632dae03f24afea05e37059338151e56ab
reyven90 <tyrondaton@gmail.com>
2018-03-15 10:45:05 -0700

    add author

---
+++ README.md
@@ -0,0 +1,3 @@
+#PHP AJAX CRUD 
+
+this is a simple PHP AJAX CRUD FOR beginers 
--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!-- 
+#*************************************************************
+#
+# SUBJECT: SIMPLE PHP AJAX CRUD
+# AUTHOR NAME: JAYVEE P AYAO 
+# CREATED DATE : March 15, 2018
+# FACEBOOK: https://www.facebook.com/jay.romantiko
+# LINKEDIN: https://www.linkedin.com/in/jayvee-ayao-487b5613a/
+#
+#*************************************************************
+ -->
 <?php require 'config/db.php' ?>
 <?php 
 
@@ -18,12 +29,7 @@
                $js=array("asset/action.js");
    	    break;
 
-          default:
-               $title = 'home';
-               $active = 'home';
-               $content = 'view/home.php';
-               $js=array("asset/action.js");
-          break;
+
    }
 
    include 'template/content.php';
